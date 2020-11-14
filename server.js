d908e55dfb950ecec0fd80fbf2775575f30b103e
Ben Brumby <bbrumby00@gmail.com>
2020-11-14 16:02:52 -0500

    Added Book and Grid components

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(expres.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googleBooks",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
